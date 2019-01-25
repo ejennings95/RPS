35fb9a02d0479a9fcc1b1c7da99bcc40881e6f2f
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-25 19:23:50 +0000

    created a form to ente login name and login button

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -6,8 +6,15 @@ feature 'You should see a welcome message at the top of the page' do
 end
 
 feature 'The homepage asks for your name' do
-  scenario 'the homepage will ask for your name to log in' do
+  scenario 'the homepage will ask for your name to login' do
     visit('/')
     expect(page).to have_content 'To login enter your name:'
+  end
+end
+
+feature 'The homepage should have a text box to fill in your name' do
+  scenario 'after the login statment there should be a text box' do
+    visit('/')
+    find('form').visible?
   end
 end
