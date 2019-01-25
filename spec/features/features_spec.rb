e002c4ca5ba11ad7d92720fb1c770cb08237c077
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-25 17:57:56 +0000

    homepage asks for user name to login

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,3 +4,10 @@ feature 'You should see a welcome message at the top of the page' do
     expect(page).to have_content 'Rock, Paper, Scissors - can you win?'
   end
 end
+
+feature 'The homepage asks for your name' do
+  scenario 'the homepage will ask for your name to log in' do
+    visit('/')
+    expect(page).to have_content 'To login enter your name:'
+  end
+end
